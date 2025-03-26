199e9f83f8ab9204d4c95a122c5b61c56cfc72ab
SH Woo <62339794+ImGdevel@users.noreply.github.com>
2025-03-26 19:26:48 +0900

    feat : url 전처리

--- src/scrapers/webtoon_list_scraper.py
+++ src/scrapers/webtoon_list_scraper.py
@@ -2,6 +2,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 from logger import Logger
 
 logger = Logger()
@@ -9,10 +10,22 @@ logger = Logger()
 class WebtoonListScraper:
     """웹툰 리스트 페이지에서 웹툰 URL을 수집하는 스크래퍼"""
 
-    WAITING_LOAD_PAGE = 5  # 로딩 시간 증가
+    WAITING_LOAD_PAGE = 5
 
     def __init__(self, driver):
         self.driver = driver
+
+    def remove_tab_param(self, url: str) -> str:
+        """URL에서 'tab' 파라미터를 제거하는 함수"""
+        parsed_url = urlparse(url)
+        query_params = parse_qs(parsed_url.query)
+
+        if 'tab' in query_params:
+            del query_params['tab']
+
+        new_query = urlencode(query_params, doseq=True)
+        new_url = parsed_url._replace(query=new_query)
+        return urlunparse(new_url)
 
     def get_webtoon_urls(self, url: str) -> list:
         """웹툰 리스트 페이지에서 웹툰 URL들을 추출한다."""
@@ -42,7 +55,7 @@ class WebtoonListScraper:
                     relative_url = link_element.get_attribute("href")
 
                     if relative_url and "/webtoon/list" in relative_url:
-                        full_url = f"https://comic.naver.com{relative_url}" if relative_url.startswith("/") else relative_url
+                        full_url = self.remove_tab_param(f"https://comic.naver.com{relative_url}" if relative_url.startswith("/") else relative_url)
                         webtoon_urls.append(full_url)
 
                 except (TimeoutException, NoSuchElementException):
